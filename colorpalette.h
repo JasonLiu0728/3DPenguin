83555413538ffa61bd149d7c20b22b409ea98a79
JasonLiu0728 <yuxi.liu2007@gmail.com>
2015-08-13 17:13:26 -0400

    I'm uploading my working copy for the first time.

---
+++ colorpalette.h
@@ -0,0 +1,22 @@
+
+
+#ifndef __COLORPALETTE_H__
+#define __COLORPALETTE_H__
+
+#include <GL/gl.h>
+
+const GLfloat COLOR_RED[] = {1.0f, 0.0f, 0.0f, 1.0f};
+const GLfloat COLOR_GREEN[] = {0.0f, 1.0f, 0.0f, 1.0f};
+const GLfloat COLOR_BLUE[] = {0.0f, 0.0f, 1.0f, 1.0f};
+const GLfloat COLOR_YELLOW[] = {1.0f, 1.0f, 0.0f, 1.0f};
+const GLfloat COLOR_PURPLE[] = {1.0f, 0.0f, 1.0f, 1.0f};
+const GLfloat COLOR_CYAN[] = {0.0f, 1.0f, 1.0f, 1.0f};
+const GLfloat COLOR_ORANGE[] = {1.0f, 0.5f, 0.0f, 1.0f};
+
+const GLfloat COLOR_WHITE[] = {1.0f, 1.0f, 1.0f, 1.0f};
+const GLfloat COLOR_BLACK[] = {0.0f, 0.0f, 0.0f};
+
+const GLfloat COLOR_DIFFUSE_DEFAULT[] = {0.8f, 0.8f, 0.8f, 1.0f};
+const GLfloat COLOR_SPECULAR_DEFAULT[] = {0.0f, 0.0f, 0.0f, 1.0f};
+
+#endif // __COLORPALETTE_H__
